188fddac91dd7dc6f3270ae97fa8e04aca657212
Zwelinzima Paul Ncube <paul.ncube14@gmail.com>
2023-06-15 16:08:12 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - a program that prints a line wiht puts function
- * Retun: Always 0 (success)
+ * main - a program that prints a line with puts function
+ * Return: Always 0 (success)
  */
 int main(void)
 {
