e728ea31098ddf000749839f2cfd3c9c245c8e0f
Zwelinzima Paul Ncube <paul.ncube14@gmail.com>
2023-06-15 15:30:25 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * main - a program that prints line with printf function
+ * return: 0 (success)
+ */
+int main (void){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
