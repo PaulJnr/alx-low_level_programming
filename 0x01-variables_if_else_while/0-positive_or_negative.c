8b4ce5a9b826511d2a4b505b6d4a2c294197b367
Zwelinzima Paul Ncube <paul.ncube14@gmail.com>
2023-06-18 15:18:48 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,6 @@
  * each time it is executed,
  * and then print whether the number stored in the variable n is positive
  * or negative.
- * 
  * Return: Always 0 (success)
 */
 int main(void)
@@ -24,11 +23,11 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-	printf("%d is zero\n", n);
+	printf("%d is negative\n", n);
 	}
 	else
 	{
-	printf("%d is negative\n", n);
+	printf("%d is zero\n", n);
 	}
 	return (0);
 }
