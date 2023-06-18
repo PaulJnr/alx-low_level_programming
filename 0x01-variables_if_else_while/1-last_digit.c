9ab780c6489d3532995da5491dff57baa06fd230
Zwelinzima Paul Ncube <paul.ncube14@gmail.com>
2023-06-18 16:05:55 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,6 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
 /**
  * main - This program will assign a random number to the variable 'n' 
  * each time it is executed,
