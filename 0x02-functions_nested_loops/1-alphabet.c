2ac817af6bdf0763dae75b602b2db705c0e559b6
Zwelinzima Paul Ncube <paul.ncube14@gmail.com>
2023-07-04 07:05:15 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,13 +8,13 @@
 
 void print_alphabet(void)
 {
-	char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}; 
+	char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	unsigned char i;
 
 	for (i = 0; i < 26; ++i)
 	{
 		_putchar(alphabet[i]);
 	};
-_putchar('\n');	
+_putchar('\n');
 }
